6852a981546959741fba74bbf3de947b1be9a38f
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2025-09-29 20:50:16 +0530

    -

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../header/Header.jsx';
+import { Outlet } from 'react-router-dom'
+import Header from './header/header.jsx';
 
 const Layout = () => {
   return (
