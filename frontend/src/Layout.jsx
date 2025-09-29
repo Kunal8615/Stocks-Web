83eb4569577c85f354aa3314d8399edf47df2abd
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2025-09-29 20:45:14 +0530

    fixed deployemt header error

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './header/header.jsx';
+import Header from '../header/Header.jsx';
 
 const Layout = () => {
   return (
